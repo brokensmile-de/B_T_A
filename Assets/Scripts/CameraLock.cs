1821ac96fc31eff9ef43232be653d262e2e585d0
Adrian Zimmer <Z.adrian1995@gmail.com>
2017-04-03 16:39:51 +0200

    Movementprototypen

---
+++ Assets/Scripts/CameraLock.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLock : MonoBehaviour {
+
+    public GameObject target;
+
+	void Update ()
+    {
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y +20, target.transform.position.z-15);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement2.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Author:Adrian Zimmer
+//Description: Movementscript für Player bei dem die Kamera immer in die richtung des Players guckt und W/A/S/D spieler relativ funktioniert
+//Date Created: 28.03.2017
+//Last edited:
+//Edited by:
+
+public class PlayerMovement2 : MonoBehaviour {
+
+    public float speed;
+    public float rotateSpeed;
+    public float jumpSpeed;
+    public float gravity;
+
+    private CharacterController controller;
+
+    public void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private Ray ray = new Ray(Vector3.zero, Vector3.down);
+    private RaycastHit hit;
+    private float facing = 0;
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Update()
+    {
+        RotatePlayer(); //Drehe Spieler wenn die maus auf der Y Achse bewegt wird
+        DoMovement();
+        AlignToGround(); //rotiere Player parallel zur oberfläche auf der er steht
+    }
+
+    private void RotatePlayer()
+    {
+        //addiere/Subtrahiere mousemovement vom yaw angle
+        facing += Input.GetAxis("Mouse X") * Time.deltaTime * rotateSpeed * 10;
+
+        //setze Yaw Rotation
+        transform.rotation = Quaternion.Euler(new Vector3(0, facing, 0));
+    }
+
+    private void DoMovement()
+    {
+        if (controller.isGrounded)
+        {
+            //berechne W/S und A/D movement
+            Vector3 forward = transform.TransformDirection(Vector3.forward) * Input.GetAxis("Vertical");
+            Vector3 right = transform.TransformDirection(Vector3.right) * Input.GetAxis("Horizontal");
+
+            //Kombiniere beide
+            moveDirection = forward + right;
+
+            //verrechne mit movementspeed
+            moveDirection *= speed * 100 * Time.smoothDeltaTime;
+
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+        }
+
+        //subtrahiere gravity
+        moveDirection.y -= gravity * Time.deltaTime;
+        //apply movement
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    private void AlignToGround()
+    {
+        ray.origin = transform.position;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Quaternion grndTilt = Quaternion.FromToRotation(Vector3.up, hit.normal);
+            transform.rotation = grndTilt * transform.rotation;
+        }
+    }
+
+}
