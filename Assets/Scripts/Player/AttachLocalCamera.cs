6929679589cf8b86728d6df700c05a3f3c22b9a2
Adrian Zimmer <Z.adrian1995@gmail.com>
2017-04-10 16:55:03 +0200

    Projekt geordnet

---
+++ Assets/Scripts/Player/AttachLocalCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+//Weist dem Lokalen Player Character während der runtime die Kamera zu
+public class AttachLocalCamera : NetworkBehaviour {
+    private CameraFollow cameraScript;
+    private GameObject player;
+	
+	void Start () {
+
+        //damit tritt das Script nur beim local player in Kraft( Ansonsten würde es die anderen Player auch beeinflussen)
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        player = this.gameObject;        
+        cameraScript = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraFollow>();        
+        cameraScript.setTarget(player);        
+    }
+	
+
+}
