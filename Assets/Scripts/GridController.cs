818760647089a2655978c244aeb3c5ddaee16b5a
Anton <antonkrapp@msn.com>
2017-04-12 16:16:50 +0200

    add level

---
+++ Assets/Scripts/GridController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class GridController : MonoBehaviour
+{
+
+    // Get a reference to the player
+    public Transform player;
+
+    void Update()
+    {
+        if (player != null)
+        {
+            GetComponent<Renderer>().sharedMaterial.SetVector("_PlayerPosition", player.position);
+        }
+        else
+        {
+            player = GameObject.Find("Player(Clone)").transform;
+        }
+    }
+}
