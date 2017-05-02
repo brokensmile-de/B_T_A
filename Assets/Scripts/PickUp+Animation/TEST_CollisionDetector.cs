8a06b18de65db7e330aafbfeffb4a56be7ff7142
Esteban Vega <stron@me.com>
2017-05-02 11:11:36 +0200

    Scrits & Prefabs

---
+++ Assets/Scripts/PickUp+Animation/TEST_CollisionDetector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST_CollisionDetector : MonoBehaviour {
+
+	public TEST_GunController gun;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.CompareTag("Gun")){
+			Debug.Log ("Gun picked up");
+			gun.PickGun(0);
+			Destroy (other.gameObject);
+		}
+	}
+}
