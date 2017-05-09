7181f9d445853d511254c8f79c5c3a9ba8e525c3
Adrian Zimmer <Z.adrian1995@gmail.com>
2017-05-09 04:09:04 +0200

    no message

---
+++ Assets/Scripts/workaround/PreloadScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PreloadScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SceneManager.LoadScene(1,LoadSceneMode.Additive);
+	}
+	public void changeScene()
+    {
+        SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(1));
+        SceneManager.I
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
