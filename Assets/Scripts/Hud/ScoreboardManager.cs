db6b65f3dee5cf9ad874923dd2dd177ca0df7cc8
Adrian Zimmer <z.adrian1995@gmail.com>
2017-06-09 14:52:29 +0200

    scoreboard anfang

--- Assets/Scripts/Hud/ScoreboardManager.cs
+++ Assets/Scripts/Hud/ScoreboardManager.cs
@@ -5,22 +5,54 @@ using UnityEngine;
 public class ScoreboardManager : MonoBehaviour {
 
     public GameObject scoreboard;
+    public GameObject scoreEntryPrefab;
+    private GameObject playerlist;
+    private bool showing;
+    public Hashtable playerList = new Hashtable();
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+        playerlist = scoreboard.transform.Find("PlayerList").gameObject;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        
-        if (Input.GetKey("tab"))
+
+        if (Input.GetKey("tab") && !showing)
+        {
             scoreboard.SetActive(true);
-        else
+            showing = true;
+            GenerateScoreboard();
+        }
+
+        else if (showing && !Input.GetKey("tab"))
         {
             if(scoreboard.activeInHierarchy)
+            {
                 scoreboard.SetActive(false);
+                showing = false;
+            }
+
         }
+    }
+
+    private void GenerateScoreboard()
+    {
+        foreach (Transform child in playerlist.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            var entry = Instantiate(scoreEntryPrefab) as GameObject;
+            entry.transform.parent = playerlist.transform;
+            entry.transform.localScale = new Vector3(1, 1, 1);
+            entry.GetComponent<EntryManager>().naeme.text = i+ "";
+        }
+
+        //Prototype.NetworkLobby.LobbyManager.s_Singleton.gameObject
     }
 }
 
