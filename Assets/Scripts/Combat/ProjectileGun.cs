77892c2035bb138a19f5bb3c90f6d52f384f9c53
Marco <kirchnermarco@gmail.com>
2017-06-13 10:44:59 +0200

    increase bullet hitbox + fix bullet rotation

--- Assets/Scripts/Combat/ProjectileGun.cs
+++ Assets/Scripts/Combat/ProjectileGun.cs
@@ -20,11 +20,17 @@ namespace Combat
 
 	    protected override void Shoot()
 	    {
+		    var baseRotation = GunController.FirePoint.rotation;
+		    baseRotation.x = 0;
+		    baseRotation.z = 0;
+		    var len = Mathf.Sqrt(baseRotation.y * baseRotation.y + baseRotation.w * baseRotation.w);
+		    baseRotation.y /= len;
+		    baseRotation.w /= len;
 	        if (WeaponController.BulletsPerShot > 1)
 	        {
 	            // Multiple bullets per shot e.g. Shotgun
 	            var bulletDistance = _curSpread / WeaponController.BulletsPerShot;
-	            var rotation = GunController.FirePoint.rotation * Quaternion.Euler(0f, -_curSpread/2, 0f);
+	            var rotation = baseRotation * Quaternion.Euler(0f, -_curSpread/2, 0f);
 	            for (int i = 0; i < WeaponController.BulletsPerShot; i++)
 	            {
                     GunController.CmdFire(rotation, WeaponController.BulletSpeed, WeaponController.MaxShotDistance);
@@ -37,7 +43,7 @@ namespace Combat
 	        {
 	            // Single bullet per shot
 	            var spread = Random.Range(-_curSpread/2, _curSpread/2);
-	            var rotation = GunController.FirePoint.rotation * Quaternion.Euler(0f, spread, 0f);
+	            var rotation = baseRotation * Quaternion.Euler(0f, spread, 0f);
 	            
 	            GunController.CmdFire(rotation, WeaponController.BulletSpeed, WeaponController.MaxShotDistance);
 	       }
