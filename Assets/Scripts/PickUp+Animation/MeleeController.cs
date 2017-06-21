e12c079c0abbe7b24468c93c3f469c1c66168e27
Esteban Vega <stron@me.com>
2017-06-21 13:17:18 +0200

    Melee damage

--- Assets/Scripts/PickUp+Animation/MeleeController.cs
+++ Assets/Scripts/PickUp+Animation/MeleeController.cs
@@ -2,24 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MeleeController : MonoBehaviour {
+
+	public int Damage;
 
 	public float punchTime;
 
 	public PlayerMovement player;
 
+	public GameObject playerOb;
+
+	public GameObject sphere;
+
+	public SphereController SController;
+
 	private float timeAcc;
 
 	private bool isPunching;
+
+	static Hitpoints enemy;
 
 
 
 	// Use this for initialization
 	void Start () {
+		sphere.SetActive (false);
 		timeAcc = punchTime;
 		isPunching = false;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if (isPunching) {
@@ -28,19 +40,23 @@ public class MeleeController : MonoBehaviour {
 				isPunching = false;
 				timeAcc = punchTime;
 				player.StopPunch();
+				sphere.SetActive (false);
 				//Debug.Log ("Punch out");
 			}
 		}
 		if(!isPunching && Input.GetKeyDown(KeyCode.E)){
+			sphere.SetActive (true);
 			isPunching = true;
 			player.Punch ();
 			//Debug.Log ("Punch in");
+			if(SController.EnemyIsInRange()){
+				Debug.Log ("Punch with Enemy in range");
+				//hier kommt der Damage handling
+				enemy = SController.HitEnemy();
+				enemy.TakeDamage (Damage, playerOb);
+			}
 		}
 	}
 
-	void OnTriggerEnter(Collider other){
-		if(other.gameObject.CompareTag("Wall") && isPunching){
-			Debug.Log ("Wall Punch");
-		}
-	}
+
 }
---
+++ Assets/Scripts/PickUp+Animation/SphereController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour {
+
+	private bool enemyInRange;
+	public GameObject LocalPlayer;
+	private GameObject enemy;
+
+	void Start(){
+		enemyInRange = false;
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Player" && other.gameObject != LocalPlayer) {
+			//Debug.Log ("its " + other.gameObject.tag);
+			enemyInRange = true;
+			enemy = other.gameObject;
+		}
+	}
+
+	public bool EnemyIsInRange(){
+		return enemyInRange;
+	}
+	public Hitpoints HitEnemy(){
+		return enemy.GetComponent<Hitpoints>();
+	}
+
+
+
+}
