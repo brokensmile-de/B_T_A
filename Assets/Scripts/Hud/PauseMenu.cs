0e030ab17e05b50447438e59d24876bd32981cc9
Adrian Zimmer <z.adrian1995@gmail.com>
2017-10-07 18:34:39 +0200

    no message

---
+++ Assets/Scripts/Hud/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+    public GameObject pauseMenu;
+    public AudioSource closeSound;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private bool showing;
+    public void openPauseMenu()
+    {
+        pauseMenu.SetActive(true);
+
+        showing = true;
+    }
+
+    public void closePauseMenu()
+    {
+        pauseMenu.SetActive(false);
+        showing = false;
+        closeSound.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown("escape") && !showing)
+        {
+            
+            openPauseMenu();
+        }
+        else if (showing && Input.GetKeyDown("escape"))
+        {
+                
+            closePauseMenu();
+        }
+
+        }
+    }
+
